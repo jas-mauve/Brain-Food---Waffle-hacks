71e63193aaa20b99baf8661abb2f20b343901250
Eri <89938497+jas-mauve@users.noreply.github.com>
2022-01-07 00:21:39 +0000

    Initial commit

---
+++ recipes.js
@@ -0,0 +1,25 @@
+var fillIn = $('.tomatoSauce');
+
+function DoSomething() {
+
+fillIn.append(`
+<div class="overlay"> →Tomato Soup recipe
+  </div>
+  <p2>Tomato soup recipe
+Ingredients:
+One red onion
+4-5 tablespoons of butter
+One can of diced tomatoes
+One cup of milk
+1 teaspoon garlic powder
+Half a teaspoon of salt
+One quarter teaspoon of pepper
+Half teaspoon of oregano flakes
+1/ 4th teaspoon basil
+1/ 4th teaspoon of paprika
+
+Turn the pot on medium heat and melt the butter put in sliced onions and cook down add can of tomatoes. Add the milk mix it and then turn it on low. Add spices and then cook on low for 40 minutes. Wants to 40 minutes is a blend it with the immersion blender and then serve.</p2>
+</div>
+`);
+event.preventDefault();
+}
